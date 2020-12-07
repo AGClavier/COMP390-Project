d20174deaf52e94eccf26b46782fdf430bcd0b3f
AGClavier <aaroncgeorge@hotmail.com>
2020-12-07 11:29:06 +0000

    Version1
    Adding project to GitHub

---
+++ Software Project/Assets/Scripts/BasicAI.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson {
+
+    public class BasicAI : MonoBehaviour
+    {
+        public NavMeshAgent agent;
+        public ThirdPersonCharacter character;
+
+        public enum State
+        {
+            PATROL,
+            CHASE
+        }
+
+        public State state;
+        private bool alive;
+
+        //Variable for patrolling
+        public GameObject[] waypoints;
+        public int waypointID = 0;
+        public float patrolSpeed = 0.5f;
+
+        //Variable for chasing
+        public float chaseSpeed = 1f;
+        public GameObject target;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+
+            state = BasicAI.State.PATROL;
+
+            alive = true;
+
+            StartCoroutine(FSM());
+        }
+
+        IEnumerator FSM()
+        {
+            while (alive)
+            {
+                switch (state)
+                {
+                    case State.PATROL:
+                        Patrol();
+                        break;
+
+                    case State.CHASE:
+                        Chase();
+                        break;
+                }
+                yield return null;
+            }
+        }
+
+        void Patrol()
+        {
+            agent.speed = patrolSpeed;
+            if (Vector3.Distance (this.transform.position, waypoints[waypointID].transform.position) >= 2)
+            {
+                agent.SetDestination(waypoints[waypointID].transform.position);
+                character.Move(agent.desiredVelocity);
+            }
+            else if (Vector3.Distance(this.transform.position, waypoints[waypointID].transform.position) <= 2)
+            {
+                waypointID += 1;
+
+                if (waypointID > waypoints.Length) 
+                {
+                    waypointID = 0;
+                }
+            }
+            else
+            {
+                character.Move(Vector3.zero);
+            }
+        }
+
+        void Chase()
+        {
+            agent.speed = chaseSpeed;
+            agent.SetDestination(target.transform.position);
+            character.Move(agent.desiredVelocity);
+        }
+
+        void OnTriggerEnter (Collider col)
+        {
+            if (col.tag == "MC")
+            {
+                state = BasicAI.State.CHASE;
+                target = col.gameObject;
+            }
+        }
+    }
+}
---
+++ Software Project/Assets/Scripts/MCMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCMovement : MonoBehaviour {
+    Vector3 original_pos;
+    Rigidbody MC;
+
+    void Start() {
+        MC = GetComponent<Rigidbody>();
+        original_pos = new Vector3(MC.transform.position.x, MC.transform.position.y, MC.transform.position.z);
+    }
+
+    void Update() {
+        float speed = 1.5f;
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w")) {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s")) {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d")) {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a")) {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.tag == "droid") {
+            MC.transform.position = original_pos;
+        }
+    }
+}
